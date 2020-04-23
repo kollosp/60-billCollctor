86bf5f17edbf3c75c97238d295fde1681950b0ab
kollosp <pparczyk@wp.pl>
2020-04-23 10:15:02 +0200

    hello endpoint

--- frontend/src/components/router.vue
+++ frontend/src/components/router.vue
@@ -3,8 +3,18 @@
 </template>
 
 <script>
+    import axios from "axios";
+
     export default {
-        name: "router"
+        name: "router",
+        mounted() {
+            console.log("page mounted")
+            axios.post("/hello").then(m => {
+                alert("hello" + m.data)
+            }).catch(e => {
+                console.error(e)
+            })
+        }
     }
 </script>
 
